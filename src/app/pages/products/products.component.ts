40085c6eb740dabb1eb174d463b31665c35fd1bc
jruizmf <jruizmf@gmail.com>
2024-11-18 22:35:26 -0700

    Starting to styling Dashboard

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { ProductTablesComponent } from '../ui-components/product-tables/product-tables.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ProductModalComponent } from 'src/app/components/modals/product-modal/product-modal.component';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [ProductTablesComponent],
+  imports: [ProductTablesComponent, MatIcon],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
+  loading:boolean = true;
+  constructor(public dialog: MatDialog) {
+    this.loading = true
+  }
+  openDialog() {
+  
+    const dialogRef = this.dialog.open(ProductModalComponent, {
+      height: "calc(100% - 30px)",
+      width: "calc(100% - 30px)",
+      maxWidth: "80%",
+      maxHeight: "800%"
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
